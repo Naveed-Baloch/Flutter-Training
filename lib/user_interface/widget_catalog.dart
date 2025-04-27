336b43af7c82313a109240989bf10b49e81e1031
Muhammad Naveed <m.naveed@texense.io>
2025-04-27 16:10:07 +0500

    Flutter : Widget catalog docs

---
+++ lib/user_interface/widget_catalog.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+
+/// Widget Catalog
+/// There are mainly Two design system for Components
+/// 1. Cupertino -> for Mac OS link https://docs.flutter.dev/ui/widgets/cupertino
+/// 2. Material mainly for Android and Web https://docs.flutter.dev/ui/widgets/material
+
+/// For Other Widgets we can check the Widget Catalog link -> https://docs.flutter.dev/ui/widgets/
