b8456bc76572333d1fa4c6ba45a97fd9c5ee369b
Muhammad Naveed <m.naveed@texense.io>
2025-05-30 20:40:33 +0500

    Flutter: Load Images and Assets

---
+++ lib/user_interface/assets_images/assets_images.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: SafeArea(
+        child: Container(color: Colors.white, child: Material(child: Assets())),
+      ),
+    ),
+  );
+}
+
+/// Loading Images
+class Assets extends StatelessWidget {
+  const Assets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () async {
+            final data = await rootBundle.loadString('assets/json/config.json');
+            final json = jsonDecode(data);
+            print(json);
+          },
+          child: const Text('Load Json File'),
+        ),
+        Image.asset('assets/images/elephant.jpg'),
+      ],
+    );
+  }
+}
