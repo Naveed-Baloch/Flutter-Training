f8d5c7e4a24b6b55a4b931bc3cf5076b08591a52
Muhammad Naveed <m.naveed@texense.io>
2025-06-29 15:41:35 +0500

    Flutter: Timer app using bloc

---
+++ lib/beyond_ui/data_backend/state_managment/bloc/projects/timer_app/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_training/beyond_ui/data_backend/state_managment/bloc/projects/timer_app/app.dart';
+
+void main() => runApp(const App());
