5acdb4a407a521157af796cbdb3b0f3a47f9f1f8
Muhammad Naveed <m.naveed32001@gmail.com>
2025-07-02 23:21:55 +0500

    Flutter: json serialization

---
+++ lib/beyond_ui/data_backend/serialization/json_and_serialization.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+/// Json & Serialization
+/// Options for serialization
+///   1. Manual
+///   2. Auto Generated Code
+///       1. Large Projects
+///     Options
+///       json_serializable
+///         @JsonSerializable()
+///         Nested Objects Serialization
+///             We need to use the explicitToJson with @JsonSerializable(explicitToJson: true)
+///              which can serialize the nested objects as well
+///       built_value
+///
+///
+///
